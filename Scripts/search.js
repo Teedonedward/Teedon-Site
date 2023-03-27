88ef8d79c4abca0a704d4186f0db5510f35ba670
Teedon <bola.com.ng123@gmail.com>
2023-03-27 01:38:48 +0100

    commits

--- Scripts/search.js
+++ Scripts/search.js
@@ -1,67 +1,21 @@
-const searchInput = document.querySelector('.search-bar input');
-const searchBtn = document.querySelector('.search-bar a');
-const suggestionsContainer = document.createElement('div');
-suggestionsContainer.classList.add('suggestions-container');
+function search() {
+    let filter = document.getElementById('search').value.toUpperCase;
 
-// Add an event listener to the search button to execute search when clicked
-searchBtn.addEventListener('click', executeSearch);
+    let item = document.querySelectorAll('.design');
 
-// Add an event listener to the search input to execute search on Enter key press
-searchInput.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-        executeSearch();
-    }
-});
+    let I = document.getElementsByTagName('h1');
 
-// Add an event listener to the search input to update suggestions in real time
-searchInput.addEventListener('input', updateSuggestions);
+    for(var i = 0; i <= 1.length; 1++) {
+        let a = item[i].getElementsByTagName('h1')[0];
 
-function executeSearch() {
-    const searchTerm = searchInput.value;
+        let value = a.innerHTML || a.innerText || a.textContent;
 
-    // Search the entire website for the entered term (e.g., using a search API)
-    // and return the results
-
-    // Display the search results on the page (e.g., in a search results section)
-}
-
-function updateSuggestions() {
-    const searchTerm = searchInput.value;
-
-    // Clear the existing suggestions
-    while (suggestionsContainer.firstChild) {
-        suggestionsContainer.removeChild(suggestionsContainer.firstChild);
-    }
-
-    // Get suggestions for the entered term (e.g., using a search API)
-    const suggestions = getSuggestions(searchTerm);
-
-    // Add the suggestions to the container
-    suggestions.forEach(function(suggestion) {
-        const suggestionElement = document.createElement('div');
-        suggestionElement.classList.add('suggestion');
-        suggestionElement.textContent = suggestion;
-        suggestionsContainer.appendChild(suggestionElement);
-    });
-
-    // Add the suggestions container to the page
-    if (suggestions.length > 0) {
-        document.body.appendChild(suggestionsContainer);
+        if(value.toUpperCase().indexOf(filter) > -1) {
+            item[i].style.display = "";
+        }
+        else
+        {
+            item[i].style.display = "none";
+        }
     }
 }
-
-function getSuggestions(searchTerm) {
-    // Get suggestions for the entered term (e.g., using a search API)
-    // and return them
-    // In this example, we'll just return some hardcoded suggestions
-    const hardcodedSuggestions = [
-        'House plans',
-        'Home designs',
-        'Kitchen remodel',
-        'Bathroom remodel',
-        'Floor plans'
-    ];
-    return hardcodedSuggestions.filter(function(suggestion) {
-        return suggestion.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
-    });
-} 
